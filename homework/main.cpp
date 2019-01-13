3ba914ac92c4f6cea431400b628cebd83df80ceb
25077667 <zxc25077667@gmail.com>
2019-01-14 01:09:47 +0800

    modify method of Q1, but bugs

--- homework/main.cpp
+++ homework/main.cpp
@@ -4,10 +4,11 @@
 #include <unistd.h>
 #include <string.h>
 #include <time.h>
+#define backoff 625
 
-void* behavior(void*);
-void create_divices(int numbers);
-bool* channels = (bool*)malloc(sizeof(bool) * 79);
+static void* behavior(void*);
+void create_divices(int device_mounts, int times);
+bool* channels = (bool*)malloc(sizeof(bool) * 80);
 int collisions=0;
 int get_random_channel(){
     srand(time(NULL)+rand());
@@ -18,33 +19,36 @@ int get_random_channel(){
 int main(){
     memset(channels,0,sizeof(channels));
     printf("1. please wait\n");
-    int i;
-    for(i=0; i<10000;i++)
-        create_divices(2);
-    printf("collisions %d in %d times.\n",collisions,i);
+    create_divices(2,16000);
+    printf("collisions %d in %d times.\n",collisions,16000);
     collisions = 0;
     return 0;
 }
 
-void create_divices(int numbers){
-    for(int i=0;i<numbers;i++){
+void create_divices(int device_mounts, int times){
+    for(int i=0;i<device_mounts;i++){
         pthread_t new_divice;
-        pthread_create(&new_divice, NULL, behavior,NULL);
+        pthread_create(&new_divice, NULL, behavior,&times);
+        printf("AAAAAAAA\n");
     }
     //pthread_join(behavior,NULL);
 }
-void* behavior(void*){
-    int used = get_random_channel();
-    if(channels[used] == true){
-        collisions ++;
-        printf("%d \n", used);
-        usleep(1);
-        behavior(NULL);
-    }
-    else{
-        channels[used] = true;
-        usleep(1);
-        channels[used] = false;
+static void* behavior(void* times){
+    int test_time = *(int*)(times);
+    //printf("%d\n",test_time);
+    usleep(backoff);
+    for(int i=0; i<test_time;i++){
+        int use = get_random_channel();
+        if(channels[use] == true){
+            collisions ++;
+            printf("%d \n", use);
+            usleep(backoff);
+        }
+        else{
+            channels[use] = true;
+            usleep(backoff);
+            channels[use] = false;
+        }
     }
     pthread_exit(NULL);
 }
