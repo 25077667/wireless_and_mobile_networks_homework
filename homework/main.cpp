a8429da251fc5918302fa5f9f37fa1d872a7d584
25077667 <zxc25077667@gmail.com>
2019-01-14 18:46:25 +0800

    because the random func. is not random enought

--- homework/main.cpp
+++ homework/main.cpp
@@ -13,13 +13,13 @@ bool* channels = (bool*)malloc(sizeof(bool) * 80);
 int collisions=0,hop=0;
 int get_random_channel(){
     srand(time(NULL)+rand());
-    return ((rand()%17)*(rand()%23)) % 80;
+    return ((rand()%17)*(rand()%23)+rand()) % 80;
 }
 
 int main(){
     memset(channels,0,sizeof(channels));
     printf("1. please wait\n");
-    int testing_length = 1;
+    int testing_length = 10;
     create_divices(2,testing_length);
     printf("collisions %d times and hopping %d times in %d seconds.\n",collisions,hop,testing_length);
 
